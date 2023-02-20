eaaaec17c5e9e5eeb74d88c3d85afe79013a6616
Vitor1452 <vitorzanetti40@gmail.com>
2023-02-20 00:30:52 -0300

    exercicio estrutura de dados

--- estrutura.js
+++ estrutura.js
@@ -12,17 +12,17 @@ const float = 10.1
 const celulas = [
     {
     lider: "Claudio",
-    membros:["valdecir","cleusa"]
+    membros:["valdecir","cleusa"],
     numero_celula: 10,
     mais_de_10: true,
     },
     {
     lider: "Luciano",
     numero_celula: 11,
-    mais_de_10: false
+    mais_de_10: false,
     membros:["valdecir","cleusa"]
 
-    }
+    },
 ]
 
 //exercicos
@@ -30,6 +30,7 @@ const celulas = [
     
 const celulas = [
     {
+        numero_da_sala: 1,
     lider: "Claudio",
     numero_celula: 10,
     mais_de_10: true,
@@ -37,32 +38,36 @@ const celulas = [
     {
         chefe: "roberto",
         funcionario: "marcos"
-    }
+    },
 
     },
     {
+        numero_da_sala: 2,
     lider: "Luciano",
     numero_celula: 11,
     mais_de_10: false,
-    membros:["marcos","romario"]
+    membros:["marcos","romario"],
     {
         chefe: "roberto",
-        funcionario: "marcos"
-    }
+        funcionario: "marcos",
+    },
 
 
     },
+
     {
+        numero_da_sala: 3,
         lider: "Luciano",
-    numero_celula: 11,
-    mais_de_10: false,
+    numero_celula; 11,
+    mais_de_10; false,
     membros:["zanaraio","claudio"]
     {
+        numero_da_sala: 4,
         chefe: "roberto",
-        funcionario: "marcos"
-    }
+        funcionario: "marcos",
+    },
 
 
     
-    }
+    },
 ]
