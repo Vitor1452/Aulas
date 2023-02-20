9bcb9712b3ca3eb2299cddc6bedcdf34aca4f99f
Vitor1452 <vitorzanetti40@gmail.com>
2023-02-20 00:17:44 -0300

    first commit

---
+++ estrutura.js
@@ -0,0 +1,57 @@
+const boolean = true 
+const string = "Vitor"
+const number = "12"
+const sem_resultado = undefined
+const nula = null
+const objeto = {}
+const array = [] //LISTAGEM!!!1
+const float = 10.1
+
+
+
+const celulas = [
+    {
+    lider: "Claudio",
+    membros:["valdecir","cleusa"]
+    numero_celula: 10,
+    mais_de_10: true,
+    },
+    {
+    lider: "Luciano",
+    numero_celula: 11,
+    mais_de_10: false
+    membros:["valdecir","cleusa"]
+
+    }
+]
+
+//exercicos
+
+    
+const celulas = [
+    {
+    lider: "Claudio",
+    numero_celula: 10,
+    mais_de_10: true,
+    membros:["valdecir","cleusa"],
+    {
+        
+    }
+
+    },
+    {
+    lider: "Luciano",
+    numero_celula: 11,
+    mais_de_10: false,
+    membros:["marcos","romario"]
+
+    },
+    {
+        lider: "Luciano",
+    numero_celula: 11,
+    mais_de_10: false,
+    membros:["zanaraio","claudio"]
+
+    
+    }
+]
